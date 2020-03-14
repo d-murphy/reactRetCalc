5b530897277af6927e792e3a6734738d5de321fd
Dan Murphy <murphy.dan.s@gmail.com>
2020-03-14 18:37:54 -0400

    logic and layout improved

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React from 'react';
 import './App.css';
 import '../node_modules/react-vis/dist/style.css';
 import {XYPlot, VerticalGridLines, HorizontalGridLines, XAxis, YAxis, Crosshair, LineSeries} from 'react-vis';
-
+  
 class App extends React.Component {
   constructor() {
     super();
     let invTotals = [];
+    let withdrawTotals = [];
     for(let i=18; i<81; i++){
       invTotals[i-18] = {x: i, y: 0}
+      withdrawTotals[i-18] = {x: i, y: 0}
     }
     this.state = {
         startingAge: 18, 
-        invStartAge: 18, 
-        invEndAge: 18,
+        invStartAge: 25, 
+        invEndAge: 55,
         oneTimeInvAge: 30, 
         withdrawAge: 55,
         startingInv: 10000,
@@ -25,110 +27,116 @@ class App extends React.Component {
     };
     this.setStateHandler = this.setStateHandler.bind(this);
     this.doCalc = this.doCalc.bind(this);
+    this.printTots = this.printTots.bind(this);
   }
   
   setStateHandler(e) {
     this.setState({[e.target.name]: e.target.value})
+    this.doCalc()
   }
   
   doCalc() {
     let newInvTots = [];
+    let newWithdrawTots = [];
     for(let i=18; i<81; i++){
       let yearTot = 0;
+      let yearWith = 0;
+      // Add interest to last years total.  Starts after year 18.
       if(i>18){
-        yearTot += newInvTots[i-19].inv * (1 + (parseFloat(this.state.invGrowth)/100))
+        yearTot += newInvTots[i-19].y * (1 + (parseFloat(this.state.invGrowth)/100))
       }
-      if(i==this.state.startingAge){
+      // withdraw funds if appropriate
+      if(i>=parseInt(this.state.withdrawAge)){
+        yearWith = newInvTots[i-19].y * (parseFloat(this.state.withRate)/100)
+        yearTot -= yearWith
+      }
+      // find the starting age and include that total
+      if(i===parseInt(this.state.startingAge)){
         yearTot += parseFloat(this.state.startingInv)
       }
-      if(i>=this.state.invStartAge & i<this.state.invEndAge){
+      // add in one time investment at appropripate year
+      if(i===parseInt(this.state.oneTimeInvAge)){
+        yearTot += parseFloat(this.state.oneTimeInv)
+      }
+      // add in annual investment for appropriate years
+      if(i>=this.state.invStartAge & i<=this.state.invEndAge){
         yearTot += parseFloat(this.state.annualInv)
       }
-      newInvTots[i-18] = {age: i, inv: yearTot}
+
+      newInvTots[i-18] = {x: i, y: yearTot}
+      newWithdrawTots[i-18] = {x: i, y: yearWith}
+      
     }
     this.setState({invTotals: newInvTots})
-    console.log(this.state.invTotals);
-
+    this.setState({withdrawTotals: newWithdrawTots})
   }
   
+  printTots() {
+    console.log( this.state.invTotals );
+    }
 
-  render() { 
-    const data = this.state.invTotals;
+   render() { 
     
-//    const data = [
-//      {x: 0, y: 8},
-//      {x: 1, y: 4},
-//      {x: 2, y: 2},
-//      {x: 3, y: 6},
-//      {x: 4, y: 1},
-//     {x: 5, y: 2},
-//      {x: 6, y: 3},
-//      {x: 7, y: 1}
-//    ];
-
-    const data2 = [
-      {x: 0, y: 4},
-      {x: 1, y: 2},
-      {x: 2, y: 6},
-      {x: 3, y: 8},
-      {x: 4, y: 10},
-      {x: 5, y: 12},
-      {x: 6, y: 13},
-      {x: 7, y: 11}
-    ];
-
     const myValues = [
       {x:5},
       {x:5}
     ]
 
     return (
-    <div className="App2">
+    <div className="App">
       <div className="Inputs">
+        <h1>{this.state.invGrowth}</h1>
         <h3>Enter your retirement plan:</h3>
-        <label for="startingAge">Starting Age: </label>
-        <input type="number" value={this.state.startingAge} onChange = {this.setStateHandler} name = "startingAge" />        
-        <label for="startingInv">Starting Investment Value: </label>
-        <input type="number" value={this.state.startingInv} onChange = {this.setStateHandler} name = "startingInv" />        
+        <label>Starting Age: 
+          <input type="number" min={18} max={80} value={this.state.startingAge} onChange = {this.setStateHandler} name = "startingAge" />        
+        </label>
+        <label>Starting Investment Value:
+          <input type="number" value={this.state.startingInv} onChange = {this.setStateHandler} name = "startingInv" />        
+        </label>
         <hr />
-        <label for="invStartAge">Annual Investment Start Age: </label>
-        <input type="number" value={this.state.invStartAge} onChange = {this.setStateHandler} name = "invStartAge" />
-        <label for="InvEndAge">Annual Investment End Age: </label>
-        <input type="number" value={this.state.invEndAge} onChange = {this.setStateHandler} name = "invEndAge" />
-        <label for="annualInv">Annual Investment Value: </label>
-        <input type="number" value={this.state.annualInv} onChange = {this.setStateHandler} name = "annualInv" />
+        <label>Annual Investment Start Age:
+          <input type="number" min={18} max={80} value={this.state.invStartAge} onChange = {this.setStateHandler} name = "invStartAge" />
+        </label>
+        <label>Annual Investment End Age:
+          <input type="number" min={18} max={80} value={this.state.invEndAge} onChange = {this.setStateHandler} name = "invEndAge" />
+        </label>
+        <label>Annual Investment Value:
+          <input type="number" value={this.state.annualInv} onChange = {this.setStateHandler} name = "annualInv" />
+        </label>
         <hr />
-        <label for="oneTimeInvAge">One Time Investment Age: </label>
-        <input type="number" value={this.state.oneTimeInvAge} onChange = {this.setStateHandler} name = "oneTimeInvAge" />
-        <label for="oneTimeInv">One Time Investment Value: </label>
-        <input type="number" value={this.state.oneTimeInv} onChange = {this.setStateHandler } name = "oneTimeInv" />
-
+        <label>One Time Investment Age:
+          <input type="number" min={18} max={80} value={this.state.oneTimeInvAge} onChange = {this.setStateHandler} name = "oneTimeInvAge" />
+        </label>
+        <label>One Time Investment Value: 
+          <input type="number" value={this.state.oneTimeInv} onChange = {this.setStateHandler } name = "oneTimeInv" />
+        </label>
         <hr />
-        <label for="withdrawAge">Withdrawal Age: </label>
-        <input type="number" value={this.state.withdrawAge} onChange = {this.setStateHandler } name = "withdrawAge" />
-        <label for="withRate">Withdrawal Percentage: </label>
-        <input type="number" value={this.state.withRate} onChange = {this.setStateHandler } name = "withRate" />
-         
+        <label>Annual Return:
+          <input type="number" value={this.state.invGrowth} onChange = {this.setStateHandler} name = "invGrowth" />
+        </label>
+        <label>Withdrawal Age:
+          <input type="number" min={18} max={80} value={this.state.withdrawAge} onChange = {this.setStateHandler } name = "withdrawAge" />
+        </label>
+        <label>Withdrawal Percentage:
+          <input type="number" value={this.state.withRate} onChange = {this.setStateHandler } name = "withRate" />
+        </label> 
         <hr />
         <button onClick={this.doCalc}>Do the thing</button>
+        <button onClick={this.printTots}>Print the Tots!</button>
       </div>
 
-      <div className="App">
-        <XYPlot height={600} width={600}>
+      <div className="Charts">
+        <XYPlot margin={{left: 100}} height={300} width={600}>
           <XAxis />
           <YAxis />
-          <VerticalGridLines />
-          <HorizontalGridLines />
-          <LineSeries data={data}  />
-          <LineSeries data={data2}  />
-          <Crosshair values={myValues}>
-            <div style={{background: 'green'}}>
-              <h3>Values of crosshair:</h3>
-              <p>Series 1: {myValues[0].x}</p>
-              <p>Series 2: {myValues[1].x}</p>
-            </div>
-          </Crosshair>
+          <LineSeries data={this.state.invTotals} />
         </XYPlot>
+        <XYPlot margin={{left: 100}} height={300} width={600}>
+          <XAxis />
+          <YAxis />
+          <LineSeries data={this.state.withdrawTotals} />
+        </XYPlot>
+
       </div>
     </div>
     );
