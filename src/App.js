de56e4552c19dc1977fd91557961fadcb6df8062
Dan Murphy <murphy.dan.s@gmail.com>
2020-03-18 22:02:53 -0400

    moved inputs to class

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
       withdrawTotals[i-18] = {x: i, y: 0}
     }
     this.state = {
-        startingAge: 18, 
+        startingAge: 20, 
         invStartAge: 25, 
         invEndAge: 55,
         oneTimeInvAge: 30, 
@@ -24,7 +24,7 @@ class App extends React.Component {
         annualInv: 0, 
         oneTimeInv: 0,
         invGrowth: 7, 
-        withRate: 4,
+        withRate: 4,  
         invTotals: invTotals,
         value: {min: 2, max: 10}
     };
@@ -82,45 +82,17 @@ class App extends React.Component {
     
     return (
     <div className="App">
-      <div className="Inputs">
-        <h3>Enter your retirement plan:</h3>
-        <p>Starting Age:</p> 
-        <InputRange maxValue={80} minValue={18} value={this.state.startingAge}  onChange= {startingAge => this.setState({ startingAge })} />
-        <label>Starting Investment Value:
-          <input type="number" value={this.state.startingInv} onChange = {this.setStateHandler} name = "startingInv" />        
-        </label>
-        <hr />
-        <label>Annual Investment Start Age:
-          <input type="number" min={18} max={80} value={this.state.invStartAge} onChange = {this.setStateHandler} name = "invStartAge" />
-        </label>
-        <label>Annual Investment End Age:
-          <input type="number" min={18} max={80} value={this.state.invEndAge} onChange = {this.setStateHandler} name = "invEndAge" />
-        </label>
-        <label>Annual Investment Value:
-          <input type="number" value={this.state.annualInv} onChange = {this.setStateHandler} name = "annualInv" />
-        </label>
-        <hr />
-        <label>One Time Investment Age:
-          <input type="number" min={18} max={80} value={this.state.oneTimeInvAge} onChange = {this.setStateHandler} name = "oneTimeInvAge" />
-        </label>
-        <label>One Time Investment Value: 
-          <input type="number" value={this.state.oneTimeInv} onChange = {this.setStateHandler } name = "oneTimeInv" />
-        </label>
-        <hr />
-        <label>Annual Return:
-          <input type="number" value={this.state.invGrowth} onChange = {this.setStateHandler} name = "invGrowth" />
-        </label>
-        <label>Withdrawal Age:
-          <input type="number" min={18} max={80} value={this.state.withdrawAge} onChange = {this.setStateHandler } name = "withdrawAge" />
-        </label>
-        <label>Withdrawal Percentage:
-          <input type="number" value={this.state.withRate} onChange = {this.setStateHandler } name = "withRate" />
-        </label> 
-        <hr />
-        <button onClick={this.doCalc}>Do the thing</button>
-        <button onClick={this.printTots}>Print the Tots!</button>
-      </div>
-
+      <Inputs startingAge={this.state.startingAge}
+              invStartAge={this.state.invStartAge}
+              invEndAge={this.state.invEndAge}
+              oneTimeInvAge={this.state.oneTimeInvAge}
+              withdrawAge={this.state.withdrawAge}
+              startingInv={this.state.startingInv}
+              annualInv={this.state.annualInv}
+              oneTimeInv={this.state.oneTimeInv}
+              invGrowth={this.state.invGrowth}
+              withRate={this.state.withRate}
+              setStateHandler={this.setStateHandler} />
       <div className="Charts">
         <XYPlot margin={{left: 100}} height={300} width={600}>
           <XAxis />
@@ -136,6 +108,51 @@ class App extends React.Component {
       </div>
     </div>
     );
+  }
+}
+
+class Inputs extends React.Component {
+  render() {
+    return( 
+      <div className="Inputs">
+      <h3>Enter your retirement plan:</h3>
+      <p>Starting Age:</p> 
+      <InputRange maxValue={80} minValue={18} value={this.props.startingAge}  onChange= {startingAge => this.setState({ startingAge })} />
+      <label>Starting Investment Value:
+        <input type="number" value={this.props.startingInv} onChange = {this.props.setStateHandler} name = "startingInv" />        
+      </label>
+      <hr />
+      <label>Annual Investment Start Age:
+        <input type="number" min={18} max={80} value={this.props.invStartAge} onChange = {this.props.setStateHandler} name = "invStartAge" />
+      </label>
+      <label>Annual Investment End Age:
+        <input type="number" min={18} max={80} value={this.props.invEndAge} onChange = {this.props.setStateHandler} name = "invEndAge" />
+      </label>
+      <label>Annual Investment Value:
+        <input type="number" value={this.props.annualInv} onChange = {this.props.setStateHandler} name = "annualInv" />
+      </label>
+      <hr />
+      <label>One Time Investment Age:
+        <input type="number" min={18} max={80} value={this.props.oneTimeInvAge} onChange = {this.props.setStateHandler} name = "oneTimeInvAge" />
+      </label>
+      <label>One Time Investment Value: 
+        <input type="number" value={this.props.oneTimeInv} onChange = {this.props.setStateHandler } name = "oneTimeInv" />
+      </label>
+      <hr />
+      <label>Annual Return:
+        <input type="number" value={this.props.invGrowth} onChange = {this.props.setStateHandler} name = "invGrowth" />
+      </label>
+      <label>Withdrawal Age:
+        <input type="number" min={18} max={80} value={this.props.withdrawAge} onChange = {this.props.setStateHandler } name = "withdrawAge" />
+      </label>
+      <label>Withdrawal Percentage:
+        <input type="number" value={this.props.withRate} onChange = {this.props.setStateHandler } name = "withRate" />
+      </label> 
+      <hr />
+      <button onClick={this.doCalc}>Do the thing</button>
+      <button onClick={this.printTots}>Print the Tots!</button>
+    </div>
+    )
   }
 }
 
